e1876e0eaafe2c8b7db4d40426df3e43d7e9ef64
“ShakilaIzadpanah” <shakila.izadpanah@mail.um.ac.ir>
2020-07-14 20:03:18 +0000

    v1.0

---
+++ Izadpanah_Purfallah.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo Please Enter  Your Request
+read request
+
+if [[ "$request" -eq "info" ]]
+then 
+  echo  "**********************************************************************************
+* A Bash Script by using the OpenStack CLI tools- Cloud Computing Course Project *
+*                                                                                *
+* Created By:                                                                    * 
+*            Shakila Izadpanah - Email: shakila.izadpanah@gmail.com              *
+*            Zahra   Purfallah - Email: zahar.purfallah@gmail.com                *
+* Version 1.0                                                                    *
+**********************************************************************************"
+fi
